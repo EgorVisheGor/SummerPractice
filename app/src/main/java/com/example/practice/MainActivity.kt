6f32f8b67003e1905d0f4cdfcb41ac471e87af35
korolkov-nat@mail.ru <BitBucketStudyKorol.ru1256>
2022-06-30 11:55:15 +0300

    Final

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -10,30 +10,48 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var name : EditText? = null
-    private var weight : EditText? = null
-    private var height : EditText? = null
-    private var age : EditText? = null
-    private var button : Button? = null
-    private var answer : TextView? = null
+    private var name: EditText? = null
+    private var weight: EditText? = null
+    private var height: EditText? = null
+    private var age: EditText? = null
+    private var button: Button? = null
+    private var answer: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val name = findViewById<EditText>(R.id.et_name).text.toString()
-        val weight = findViewById<EditText>(R.id.et_weight).text.toString()
-        val height = findViewById<EditText>(R.id.et_heiht).text.toString()
-        val age  = findViewById<EditText>(R.id.et_age).text.toString()
-        val answer : TextView = findViewById(R.id.tv_answer)
-        val button : Button = findViewById(R.id.btn_calculate)
+        val nameField = findViewById<EditText>(R.id.et_name).text
+        val weightField = findViewById<EditText>(R.id.et_weight).text
+        val heightField = findViewById<EditText>(R.id.et_heiht).text
+        val ageField = findViewById<EditText>(R.id.et_age).text
+        val answer: TextView = findViewById(R.id.tv_answer)
+        val button: Button = findViewById(R.id.btn_calculate)
 
         button.setOnClickListener() {
-            if (name.isEmpty() || name.length >= 50 || height < 0 || height > 250 || weight < 0 || weight > 250 || age < 0 || age > 150) {
-                answer.setText("Данные введены некорректно")
+            var name = nameField.toString();
+            var weight = weightField.toString().toIntOrNull();
+            var height = heightField.toString().toIntOrNull();
+            var age = ageField.toString().toIntOrNull();
+
+            if (name.isEmpty()
+                || name.length >= 50
+                || height == null
+                || weight == null
+                || age == null
+                || height < 0
+                || height > 250
+                || weight < 0
+                || weight > 250
+                || age < 0
+                || age > 150
+            ) {
+                answer.text = "Данные введены некорректно"
             } else {
-                val result : Double = weight/height/3.14 - age/age- name.length/name.length + 2
-                answer.setText("Примерный обхват вашей $name талии $result")
+                val result: Double =
+                    weight / height / 3.14 - age / age - name.length / name.length + 2
+                answer.text = "Примерный обхват вашей $name талии $result"
             }
         }
 
